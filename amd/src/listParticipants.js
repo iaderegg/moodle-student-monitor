befc46b416c6850abddd9490f336f54a4cc6a67c
iaderegg <iadergg@gmail.com>
2021-05-13 16:32:23 -0500

    Add amd javascript for List of Participants

---
+++ amd/src/listParticipants.js
@@ -0,0 +1,36 @@
+/*eslint no-unused-vars: ["error", { "args": "none" }]*/
+define(['jquery',
+    'core/ajax',
+    'core/templates',
+    'report_studentmonitor/jszip',
+    'report_studentmonitor/pdfmake',
+    'report_studentmonitor/jquery.dataTables',
+    'report_studentmonitor/dataTables.bootstrap4',
+    'report_studentmonitor/autoFill.bootstrap4',
+    'report_studentmonitor/dataTables.buttons',
+    'report_studentmonitor/buttons.bootstrap4',
+    'report_studentmonitor/buttons.colVis',
+    'report_studentmonitor/buttons.html5',
+    'report_studentmonitor/buttons.print'
+    // 'report_studentmonitor/sweetalert',
+], function ($,
+    ajax,
+    templates,
+    jszip,
+    pdfmake,
+    dataTable,
+    bootstrapDataTables,
+    autoFill,
+    buttonsDatatables,
+    buttonsBootstrap,
+    buttonscolVis,
+    buttonsHtml5,
+    buttonsPrint
+    //Swal
+) {
+
+    return {
+        init: function () {
+        }
+    };
+});
